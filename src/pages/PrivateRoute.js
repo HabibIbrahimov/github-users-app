7f570f2a7613ce3a06258dda9594bca45ee61841
HabibIbrahimov <habibyi@code.edu.az>
2023-02-02 20:46:40 +0400

    first commit

---
+++ src/pages/PrivateRoute.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const PrivateRoute = () => {
+  return <h2>private route component</h2>;
+};
+export default PrivateRoute;
